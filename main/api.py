a99fafb7181de2e41e447b792761d7c883978d0f
Mateusz Maćkowski <m4tx@m4tx.pl>
2017-02-03 11:05:39 +0100

    Add docs to api.py

--- main/api.py
+++ main/api.py
@@ -6,6 +6,13 @@ from usosapi import USOSAPIConnection
 
 class API(USOSAPIConnection):
     def get_student(self, student_id):
+        """Query the student_id in the USOS database.
+
+        :param str student_id: student ID to query
+        :return: dictionary containing student's first and last name if they
+            were found in the database; `None` otherwise
+        :rtype: dict|None
+        """
         data = {
             'lang': 'pl',
             'fields': 'items[user[first_name|last_name]]',
@@ -17,7 +24,16 @@ class API(USOSAPIConnection):
             return response['items'][0]['user']
         return None
 
-    def process_id_list(self, id_list: str, student_id_regex: str):
+    def process_id_list(self, id_list, student_id_regex):
+        """Go through lines in id_list and replace student IDs with their names.
+
+        :param str id_list: list of student IDs, one per line
+        :param str student_id_regex: regex that matches student ID in each line.
+            If the regex cannot be matched or a student is not found in the
+            cache nor the database, then a line is left without changes.
+        :return: processed string
+        :rtype: str
+        """
         prog = re.compile(student_id_regex)
         rv = []
         for line in id_list.splitlines():
