c08ecf732d547a9ee547f1a5bdba0d22a2efb5b5
Mateusz Maćkowski <m4tx@m4tx.pl>
2017-02-03 10:43:04 +0100

    Add regex validation

--- main/forms.py
+++ main/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 
+from main.fields import RegexField
+
 
 class IndexForm(forms.Form):
     id_list = forms.CharField(
         widget=forms.Textarea, label='ID List',
         help_text='List of students IDs to query, one per line.')
-    student_id_regex = forms.CharField(
+    student_id_regex = RegexField(
         label='Student ID regex',
         help_text='Regular expression used to match the student ID in each '
                   'line. If cannot match (or a student is not found in the '
